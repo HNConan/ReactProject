6236ee9d6fec9fae6a0fd6a58b2c45f5517bf39a
Paul77700 <paulcompagnon@icloud.com>
2023-06-02 14:04:51 +0200

    HealthGoals Form

--- Project/Screen/HealthGoals.js
+++ Project/Screen/HealthGoals.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
-import {StyleSheet, View,Text,TextInput, Button } from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import React, {useState, useEffect } from 'react';
+import {StyleSheet, View,Text,TextInput, Button, ScrollView } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 import { TouchableWithoutFeedback } from 'react-native';
 import { Keyboard } from 'react-native';
 
@@ -16,6 +16,16 @@ const HealthGoals = () => {
   const [weight, setWeight] = useState('');
   const [activityLevel, setActivityLevel] = useState('');
   const [healthGoal, setHealthGoal] = useState('');
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    // Vérifier si tous les champs sont remplis pour activer/désactiver le bouton de soumission
+    if (age && gender && height && weight && activityLevel && healthGoal) {
+      setIsFormValid(true);
+    } else {
+      setIsFormValid(false);
+    }
+  }, [age, gender, height, weight, activityLevel, healthGoal]);
 
   const handleSubmit = () => {
     console.log('Form submitted!');
@@ -28,73 +38,73 @@ const HealthGoals = () => {
   };
 
   return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+
     <TouchableWithoutFeedback onPress={handleDismissKeyboard}>
     <View style={styles.container}>
       <Text style={styles.label}>Votre Age:</Text>
       <TextInput style={styles.input} value={age} onChangeText={text => setAge(text)} keyboardType="numeric"/>
 
-      <Text style={styles.label}>Votre Sexe:</Text>
-      <Picker
-        style={styles.picker}
-        selectedValue={activityLevel}
-        onValueChange={(itemValue) => setActivityLevel(itemValue)}
-      >
-        <Picker.Item label="Homme" value="Male" />
-        <Picker.Item label="Femme" value="Female" />
-        <Picker.Item label="Non Binaire" value="binary" />
-      </Picker>
-
       <Text style={styles.label}>Votre Taille :</Text>
-      <TextInput
-        style={styles.input}
-        value={height}
-        onChangeText={text => setHeight(text)}
-        keyboardType="numeric"
-      />
+      <TextInput style={styles.input} value={height} onChangeText={text => setHeight(text)} keyboardType="numeric" />
 
       <Text style={styles.label}>Votre Poids:</Text>
-      <TextInput
-        style={styles.input}
-        value={weight}
-        onChangeText={text => setWeight(text)}
-        keyboardType="numeric"
+      <TextInput style={styles.input} value={weight} onChangeText={text => setWeight(text)} keyboardType="numeric"/>
+
+      <Text style={styles.label}>Votre Sexe:</Text>
+      <RNPickerSelect
+        placeholder={{ label: 'Sélectionnez votre sexe', value: null }}
+        items={[
+          { label: 'Homme', value: 'Male' },
+          { label: 'Femme', value: 'Female' },
+          { label: 'Autre', value: 'other' },
+        ]}
+        style={pickerSelectStyles}
+        value={gender}
+        onValueChange={(value) => setGender(value)}
       />
 
       <Text style={styles.label}>Niveau d'activité:</Text>
-      <Picker
-        style={styles.picker}
-        selectedValue={activityLevel}
-        onValueChange={(itemValue) => setActivityLevel(itemValue)}
-      >
-        <Picker.Item label="Sedentary" value="sedentary" />
-        <Picker.Item label="Light Exercise" value="light exercise" />
-        <Picker.Item label="Moderate Exercise" value="moderate exercise" />
-        <Picker.Item label="Heavy Exercise" value="heavy exercise" />
-        <Picker.Item label="Extra Active" value="extra active" />
-      </Picker>
+      <RNPickerSelect
+        placeholder={{ label: 'Sélectionnez votre niveau d\'activité', value: null }}
+        items={[
+          { label: 'Exercices légers', value: 'light_exercise' },
+          { label: 'Exercices modérés', value: 'moderate_exercise' },
+          { label: 'Exercices difficiles', value: 'heavy_exercise' },
+          { label: 'Sportif de haut niveau', value: 'extra_active' },
+        ]}
+        style={pickerSelectStyles}
+        value={activityLevel}
+        onValueChange={(value) => setActivityLevel(value)}
+      />
 
       <Text style={styles.label}>Objectif de santé:</Text>
-      <Picker
-        style={styles.picker}
-        selectedValue={healthGoal}
-        onValueChange={(itemValue) => setHealthGoal(itemValue)}
-      >
-        <Picker.Item label="Perte de poids" value="weight loss" />
-        <Picker.Item label="Maintien de poids" value="weight maintenance" />
-        <Picker.Item label="Gain de Poids" value="weight gain" />
-      </Picker>
-
-      <Button title="Commencer" onPress={handleSubmit} />
+      <RNPickerSelect
+        placeholder={{ label: 'Sélectionnez votre objectif de santé', value: null }}
+        items={[
+          { label: 'Perte de poids', value: 'weight_loss' },
+          { label: 'Maintien de poids', value: 'weight_maintenance' },
+          { label: 'Gain de Poids', value: 'weight_gain' },
+        ]}
+        style={pickerSelectStyles}
+        value={healthGoal}
+        onValueChange={(value) => setHealthGoal(value)}
+      />
+      <Button title="Commencer" onPress={handleSubmit} disabled={!isFormValid} />
     </View>
     </TouchableWithoutFeedback>
-
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
     padding: 20,
+    minHeight: '100%', // ou utilisez flex: 1 si vous préférez
   },
   label: {
     fontSize: 16,
@@ -107,7 +117,28 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 10,
   },
-  picker: {
+});
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30,
+    marginBottom: 10,
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30, 
     marginBottom: 10,
   },
 });
