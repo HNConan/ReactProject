3c03dbff74a8cadda4fab09ed7bca64d43156fc9
Paul77700 <paulcompagnon@icloud.com>
2023-06-02 15:42:03 +0200

    Health Goals

--- Project/Screen/HealthGoals.js
+++ Project/Screen/HealthGoals.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect } from 'react';
 import {Picker} from '@react-native-picker/picker';
-import {StyleSheet, View,Text,TextInput, Button, ScrollView } from 'react-native';
+import Modal from 'react-native-modal';
+import {StyleSheet, View,Text,TextInput, Button, ScrollView, Alert } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native';
 import { Keyboard } from 'react-native';
 
@@ -17,6 +18,35 @@ const HealthGoals = () => {
   const [activityLevel, setActivityLevel] = useState('');
   const [healthGoal, setHealthGoal] = useState('');
   const [isFormValid, setIsFormValid] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [result, setResult] = useState(0);
+
+  const activityLevelValues = {
+    sedentary: 1.2,
+    light_exercise: 1.375,
+    moderate_exercise: 1.55,
+    heavy_exercise: 1.725,
+    extra_active: 1.9,
+  };
+
+  const weightGoals = {
+    weight_loss: -500,
+    weight_gain: 500,
+    weight_maintenance: 0,
+  };
+
+  calculateBMR = () => { 
+      const activityLevelValue = activityLevelValues[activityLevel];
+      const weightGoalValue = weightGoals[healthGoal];
+      if(gender === "Male"){
+        return ((88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)) * activityLevelValue) + weightGoalValue;
+      }else if(gender === "Female"){
+        return ((447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)) * activityLevelValue) + weightGoalValue;
+      }else{
+        return null;
+      }
+  };
+
 
   useEffect(() => {
     // Vérifier si tous les champs sont remplis pour activer/désactiver le bouton de soumission
@@ -35,6 +65,11 @@ const HealthGoals = () => {
     console.log('Weight:', weight);
     console.log('Activity Level:', activityLevel);
     console.log('Health Goal:', healthGoal);
+    
+    const bmr = calculateBMR();
+    console.log('BMR:', bmr);
+    setResult(bmr);
+    setIsModalVisible(true);
   };
 
   return (
@@ -45,21 +80,21 @@ const HealthGoals = () => {
       <Text style={styles.label}>Votre Age:</Text>
       <TextInput style={styles.input} value={age} onChangeText={text => setAge(text)} keyboardType="numeric"/>
 
-      <Text style={styles.label}>Votre Taille :</Text>
+      <Text style={styles.label}>Votre Taille (cm):</Text>
       <TextInput style={styles.input} value={height} onChangeText={text => setHeight(text)} keyboardType="numeric" />
 
-      <Text style={styles.label}>Votre Poids:</Text>
+      <Text style={styles.label}>Votre Poids (kg):</Text>
       <TextInput style={styles.input} value={weight} onChangeText={text => setWeight(text)} keyboardType="numeric"/>
 
       <Text style={styles.label}>Votre Sexe:</Text>
       <Picker style={styles.picker} selectedValue={gender} onValueChange={(itemValue) => setGender(itemValue)}>
         <Picker.Item label="Homme" value="Male" />
         <Picker.Item label="Femme" value="Female" />
-        <Picker.Item label="Autre" value="other" />
       </Picker>
 
       <Text style={styles.label}>Niveau d'activité:</Text>
       <Picker style={styles.picker} selectedValue={activityLevel} onValueChange={(itemValue) => setActivityLevel(itemValue)}>
+        <Picker.Item label="Sédentaire" value="sedentary" />
         <Picker.Item label="Exercices légers" value="light_exercise" />
         <Picker.Item label="Exercices modérés" value="moderate_exercise" />
         <Picker.Item label="Exercices difficiles" value="heavy_exercise" />
@@ -75,6 +110,14 @@ const HealthGoals = () => {
       </Picker>
 
       <Button title="Commencer" onPress={handleSubmit} disabled={!isFormValid} />
+
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalText}>Votre BMR est :</Text>
+          <Text style={styles.modalText}>{result}</Text>
+          <Button title="Fermer" onPress={() => setIsModalVisible(false)} />
+        </View>
+      </Modal>
     </View>
     </TouchableWithoutFeedback>
     </ScrollView>
@@ -102,6 +145,16 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   picker: {
+    marginBottom: 10,
+  },
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
     marginBottom: 10,
   },
 });
