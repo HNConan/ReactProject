cdbd0c2f904f1f601135b4f4565e714c64312a5f
HNConan <hadrien.conan@edu.esiee.fr>
2023-06-16 10:40:51 +0200

    Improve UX

--- Project/Screen/DayMeals.js
+++ Project/Screen/DayMeals.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import ViewMeals from './ViewMeals';
 
+
 const DayMeals = ({dayMenu, onRemoveFood, onAddFood }) => {
     const calculateTotalCalories = (meals) => {
         let totalCalories = 0;
@@ -14,18 +15,24 @@ const DayMeals = ({dayMenu, onRemoveFood, onAddFood }) => {
         });
         return totalCalories;
       };
-    
+      const caloPerMeal = (meals) => {
+        return calculateTotalCalories({ meals: [...meals] });
+      };
       return (
         <View>
-          <ViewMeals meals={dayMenu.Breakfast}  onAddFood={onAddFood} onRemoveFood={onRemoveFood} title="Breakfast" />
-          <ViewMeals meals={dayMenu.Lunch}  onAddFood={onAddFood} onRemoveFood={onRemoveFood} title="Lunch" />
-          <ViewMeals meals={dayMenu.Dinner}  onAddFood={onAddFood} onRemoveFood={onRemoveFood} title="Dinner" />
-          <ViewMeals meals={dayMenu.Snack}  onAddFood={onAddFood} onRemoveFood={onRemoveFood} title="Snack" />
+          <ViewMeals meals={dayMenu.Breakfast}  onAddFood={onAddFood} onRemoveFood={onRemoveFood} title="Breakfast" calo={caloPerMeal(dayMenu.Breakfast)} />
+          
+          <ViewMeals meals={dayMenu.Lunch}  onAddFood={onAddFood} onRemoveFood={onRemoveFood} title="Lunch"  calo={caloPerMeal(dayMenu.Lunch)} />
+          <ViewMeals meals={dayMenu.Dinner}  onAddFood={onAddFood} onRemoveFood={onRemoveFood} title="Dinner" calo={caloPerMeal(dayMenu.Dinner)} />
+          <ViewMeals meals={dayMenu.Snack}  onAddFood={onAddFood} onRemoveFood={onRemoveFood} title="Snack" calo={caloPerMeal(dayMenu.Snack)} />
           
           <Text style={styles.totalCalories}>
-            Total Calories (kcal): {calculateTotalCalories({'Breakfast' :[...dayMenu.Breakfast], 'Lunch' : [...dayMenu.Lunch], 'Dinner' : [...dayMenu.Dinner], 'Snack' : [...dayMenu.Snack]})}
-
+            Total Calories (kcal) : {} {calculateTotalCalories({'Breakfast' :[...dayMenu.Breakfast], 'Lunch' : [...dayMenu.Lunch], 'Dinner' : [...dayMenu.Dinner], 'Snack' : [...dayMenu.Snack]})}
           </Text>
+        
+  
+
+
     </View>
   );
 };
--- Project/Screen/ViewMeals.js
+++ Project/Screen/ViewMeals.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
 import { Button, TextInput, IconButton } from "react-native-paper";
 
-const ViewMeals = ({ meals, title, onRemoveFood, onAddFood }) => {
+const ViewMeals = ({ meals, title, onRemoveFood, onAddFood,calo }) => {
   return (
     <View>
+        <View style={styles.mealTitleContainer}>
       <Text style={styles.mealTitle}>{title}:</Text>
+      <Text style={styles.caloPerMeal} >{calo}</Text>
+      </View>
+
+
       <View style={styles.mealItemsContainer}>
         {meals.map((meal, index) => (
           <View key={index} style={styles.mealItemContainer}>
@@ -16,15 +20,16 @@ const ViewMeals = ({ meals, title, onRemoveFood, onAddFood }) => {
                 onPress={() => onRemoveFood(title, index)}
                 icon="close"
                 
+                
               />
             </View>
           </View>
         ))}
         <View style={styles.ButtonContainer}>
           <Button
-            mode="contained"
+            mode="text"
+            
             onPress={() => onAddFood(title)}
-            style={styles.Button}
             labelStyle={styles.ButtonLabel}
           >
             Add Food
@@ -56,13 +61,25 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   ButtonContainer: {
-    marginLeft: 10, 
+    marginLeft: 10,
+    width: 100,
+  
   },
-  Button: {
-    backgroundColor: 'transparent',
-  },
+
   ButtonLabel: {
     color: 'blue',
+  },
+  caloPerMeal:
+  {
+    marginRight: 10,
+
+  },
+  mealTitleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+    marginRight: 10,
   },
 });
 export default ViewMeals;
